dda07d6c0ba2b94420996532d422a26b3e01e511
Turma 28 - Desenvolvimento Web - Front End <t028@escola.to.senac.br>
2025-04-04 20:31:46 -0300

    esqueleto

---
+++ script.js
@@ -0,0 +1,22 @@
+function mostrarBody1() {
+    let painelBtn = document.getElementById('painelBtn');
+    let estoqueBtn = document.getElementById('estoqueBtn');
+    let body1 = document.getElementById('body1');
+    let body2 = document.getElementById('body2');
+
+    body1.style.display = 'block';
+    body2.style.display = 'none';
+}
+
+
+function mostrarBody2() {
+    let painelBtn = document.getElementById('painelBtn');
+    let estoqueBtn = document.getElementById('estoqueBtn');
+    let body1 = document.getElementById('body1');
+    let body2 = document.getElementById('body2');
+    
+    body1.style.display = 'none';
+    body2.style.display = 'block';
+}
+
+
